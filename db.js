83800b6bcf792c791dddb1b4f70f797e3a66525f
Alex-Lee-Myers <alexander.l.myers@gmail.com>
2021-12-14 14:19:12 -0500

    committing before checkout to personal branch for feature update

--- db.js
+++ db.js
@@ -5,15 +5,15 @@ const { Sequelize } = require('sequelize');
 //*  
 //! HEROKU DATABASE
 //TODO: Enable Heroku database by uncommenting the following object.
-const db = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
-});
+// const db = new Sequelize(process.env.DATABASE_URL, {
+//     dialect: 'postgres',
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false
+//         }
+//     }
+// });
 //TODO: Uncomment the Heroku Database before committing. Comment the Localhost Database. Message Adam for testing Heroku side(?) <-- Not sure how to incorporate Heroku test fully. 
 //! END OF HEROKU DATABASE
 
@@ -21,9 +21,9 @@ const db = new Sequelize(process.env.DATABASE_URL, {
 
 //TODO LOCAL DATABASE
 //!  START OF LOCAL DATABASE
-// const db = new Sequelize(process.env.DATABASE_URL, {
-//     dialect: 'postgres',
-// });
+const db = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+});
 //TODO Change back to Heroku Database before committing!
 //! END OF LOCAL DATABASE
 
