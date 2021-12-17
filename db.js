7273c827c8dd9d539f0c0dba14edf6f0d93255a7
coderap931 <88391040+coderap931@users.noreply.github.com>
2021-12-17 12:30:47 -0500

    fixed for heroku deployment

--- db.js
+++ db.js
@@ -6,15 +6,15 @@ const { Sequelize } = require('sequelize');
 //! |  Activate your database profile. |
 //TODO To activate your databate profile, call in the correct database profile via "process.env.${const of your database password}"
 //TODO When done for the day, replace line 10 of this file with line 9 uncommented. This allows us to not constantly resolve conflicts when merging.
-const db = new Sequelize(`postgres://postgres:${process.env.AdamPW}@localhost:5432/${process.env.dbName}`);
-// const db = new Sequelize(process.env.DATABASE_URL, {
-//     dialect: 'postgres',
-//     dialectOptions: {
-//         ssl: {
-//             require: true,
-//             rejectUnauthorized: false
-//         }
-//     }
-// });
+// const db = new Sequelize(`postgres://postgres:${process.env.AdamPW}@localhost:5432/${process.env.dbName}`);
+const db = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+});
 
 module.exports = db;
